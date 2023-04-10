87216dfaf55230fe5a7be3f5397c5067bdace239
kannaujiya2501 <kannaujiyavishal12@gmail.com>
2023-04-10 17:39:32 +0530

    some component added

--- shoppingcart/E-shoping/src/app/app-routing.module.ts
+++ shoppingcart/E-shoping/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'header',component:HeaderComponent},
+{path:'footer',component:FooterComponent},
+{path:'home',component:HomeComponent},
+{path:'login',component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
